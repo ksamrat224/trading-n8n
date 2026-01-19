f80ebeb6b28be12f7e06c0fbc9bf48cb018f226c
Samrat <ksamrat224@gmail.com>
2026-01-19 20:46:08 +0545

    feat: add trigger selection UI to TriggerSheet component

--- client/src/component/TriggerSheet.tsx
+++ client/src/component/TriggerSheet.tsx
@@ -10,13 +10,37 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
 
 type NodeMetaData = any;
+const SUPPORTED_TRIGGERS: [
+  {
+    id: "timer";
+    title: "Timer";
+    description: "Run this trigger every x seconds/minutes.";
+  },
+  {
+    id: "price-trigger";
+    title: "Price Trigger";
+    description: "Runs whenever the trigger  price goes above or below a certain number for an asset.";
+  },
+];
+
 export const TriggerSheet = ({
   onSelect,
 }: {
   onSelect: (kind: NodeKind, metadata: NodeMetaData) => void;
 }) => {
+  const [metadata, setMetadata] = useState({});
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -27,6 +51,28 @@ export const TriggerSheet = ({
           <SheetTitle>Select Trigger</SheetTitle>
           <SheetDescription>
             Select the type of trigger that you need.
+            <Select>
+              <SelectTrigger className="w-45">
+                <SelectValue placeholder="Select a fruit" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  {SUPPORTED_TRIGGERS.map(({ id, title }) => (
+                    <>
+                      <SelectLabel>{title}</SelectLabel>
+                      <SelectItem
+                        onSelect={() => onSelect(id, metadata)}
+                        value={id}
+                      >
+                        {title}
+                      </SelectItem>
+                    </>
+                  ))}
+
+                  <SelectItem value="banana">Banana</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </SheetDescription>
         </SheetHeader>
 
