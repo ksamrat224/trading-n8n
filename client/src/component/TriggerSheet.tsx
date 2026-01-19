64e96d8504cfb10f4e9703bc461c8ccafca47af9
Samrat <ksamrat224@gmail.com>
2026-01-19 20:26:00 +0545

    feat: add select, label, input components and TriggerSheet using Radix UI

--- client/src/component/TriggerSheet.tsx
+++ client/src/component/TriggerSheet.tsx
@@ -1,1 +1,42 @@
-export const TriggerSheet = ({}) => {};
+import { Button } from "@/components/ui/button";
+import type { NodeKind } from "./CreateWorkflow";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+type NodeMetaData = any;
+export const TriggerSheet = ({
+  onSelect,
+}: {
+  onSelect: (kind: NodeKind, metadata: NodeMetaData) => void;
+}) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Select Trigger</SheetTitle>
+          <SheetDescription>
+            Select the type of trigger that you need.
+          </SheetDescription>
+        </SheetHeader>
+
+        <SheetFooter>
+          <Button type="submit">Save changes</Button>
+          <SheetClose asChild>
+            <Button variant="outline">Close</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
